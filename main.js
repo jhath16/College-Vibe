693b94224bfca47408144115d15b99881c8acc19
jhath16 <hathaway.justin@gmail.com>
2015-06-12 16:42:39 -0400

    adds assets folder

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var College = Backbone.Model.extend({
+  initialize: function () {
+
+  },
+
+  defaults : {
+    name:'',
+    address:'',
+    //more to come
+  }
+});
+
+var ApplicationView = Backbone.View.extend({
+  initialize:function () {
+
+  },
+
+  render: function () {
+
+  }
+});
+
+
+var Router = Backbone.Router.extend({});
+
+$(document).ready(function () {
+  //Glue code
+});
