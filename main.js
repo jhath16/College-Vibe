d985dd18c6da4acd41a72c2eb17a46ca3d83f24e
jhath16 <hathaway.justin@gmail.com>
2015-06-17 23:39:23 -0400

    carries the searchDropdown functionality from old project

--- main.js
+++ main.js
@@ -39,42 +39,85 @@ var College = Backbone.Model.extend({
   }
 });
 
+var IndexView = Backbone.View.extend({
+  template: _.template($('#index-route').text()),
+
+  render : function () {
+    this.$el.html(this.template());
+    $('#application').append(this.el);
+    return this;
+  },
+
+  events: {
+    'keyup input' : 'searchDropdown'
+  },
+
+  searchDropdown: function (e) {
+    var self = this,
+				queryString = e.currentTarget.value.toLowerCase(),
+        qLength = queryString.length;
+
+    console.log(queryString);
+		if (e.keyCode != 16) { //excludes the shift key, might not be necessary
+      var dataArray = this.model;
+			//Define the filter callback
+			function filterFunction (i) {
+				return i.get('Name').slice(0,qLength).toLowerCase() == queryString ? true : false;
+			};
+			//filter through the data provided
+			var matchedQuery = dataArray.filter(filterFunction);
+			//matchedQuery now has the array of matching objects
+
+			//clear the search options
+			$('.college-search').empty();
+
+			//only run this is the input isn't empty so we avoid matching all the data
+			if (qLength != '') {
+
+			//append the new search options
+				matchedQuery.forEach(
+					//will be changed to a simple class instead of applying style w/ jQuery
+					function (i) {
+						var newDiv = document.createElement('div');
+						newDiv.style.color = 'white';
+						newDiv.style.padding = '5px 0';
+						newDiv.textContent = i.get('Name');
+						$('.college-search').append(newDiv);
+				});
+			}
+		}
+  }
+});
+
+var CollegeCollection = Parse.Collection  //Does Parse have a collection?
 
 var Router = Backbone.Router.extend({
   routes: {
     '' : 'home',
-    'something' : 'something'
   },
 
   home: function () {
-    renderToApplication($('#index-route'));
-    console.log('index route loaded');
+    console.log('index route function fired');
   },
-
-  something: function () {
-    console.log('something route loaded');
-  }
 });
 
 var router = new Router();
 
 $(document).ready(function () {
   Backbone.history.start();
+
+  var query = new Parse.Query("College");
+  query.find().then(function (e){
+    var indexView = new IndexView({model:e});
+    indexView.render();
+  })
 });
-
-
-var CollegeCollection = Parse.Collection.extend({
-  query: function () {
-    return new Parse.Query('College').find();
-  }
-})
-
 /*
 
 ********  To Do  **************
 
 1. Writing out a render function that will take a template from the dom and
-render it to a specified container (exactly like we did in TIY I guess). DONE
+render it to a specified container (exactly like we did in TIY I guess).
 
 2.Map Out the Router and organize the rendering of templates accordingly ISH
 
@@ -84,6 +127,11 @@ all dynamic templates to.
 
 4. External templates for ease in development?
 -Probably not at least for the moment but still an option.
+-How would you go about working with external templates?...
+
+5. Should we make it so that each view renders itself on instantiation?
+-Router should control the rendering of routes... Just instantiate/.remove() items?
+-^ This sounds like the best method of doing so.
 
 
 
